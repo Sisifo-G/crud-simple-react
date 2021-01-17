9ddb1b62ebe40702c9a3a834da0812e406a50a06
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-17 15:44:03 -0500

    Tareas agregadas dinámicamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import shortid from 'shortid';
 
 
 function App() {
 
   const [ tarea, setTarea ] = useState('');
+  const [ tareas, setTareas ] = useState([]);
 
   const agregarTarea = e =>{
     e.preventDefault();
+
+    // Validar
+    if(!tarea.trim()){
+      console.log('Sin tarea');
+      return;
+    }
+    console.log(tarea);
+    setTareas([
+      ...tareas,
+      {id: shortid.generate(), nombreTarea: tarea}
+    ])
+
+    setTarea('');
   }
 
   return (
@@ -17,11 +32,16 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de tarea</span>
+          {
+            tareas.map(item =>(
+              <li className="list-group-item" key={item.id}>
+              <span className="lead"> {item.nombreTarea} </span>
               <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
               <button className="btn btn-warning btn-sm float-end">Editar</button>
             </li>
+            ))
+          }
+            
           </ul>
         </div>
         <div className="col-4">
@@ -32,6 +52,7 @@ function App() {
               className="form-control mb-2"
               placeholder="Ingrese la Tarea"
               onChange={ e => setTarea(e.target.value) }
+              value={tarea}
             />
             <button className="btn btn-dark w-100" type="submit">Agregar Tarea</button>
           </form>
